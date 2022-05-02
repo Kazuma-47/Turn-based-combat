64ff41a8a9bf1c404baa4469ac902e50375a3cb7
Kazuma-47 <70643013+Kazuma-47@users.noreply.github.com>
2022-05-02 09:12:02 +0200

    moved text mesh pro folder added post combat

---
+++ Assets/scripts/Player/character/Ability/Moves.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moves : Ability
+{
+    private string name;
+    private float damage;
+    private string Discription;
+    
+    public override void Use()
+    {
+              
+    }
+}
