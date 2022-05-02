4257089d96f1dd6b79e026653e2c43f308d9b6ff
Kazuma-47 <70643013+Kazuma-47@users.noreply.github.com>
2022-05-02 10:20:12 +0200

    added: can resize team if needed

--- Assets/scripts/Combat/TeamManager.cs
+++ Assets/scripts/Combat/TeamManager.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class TeamManager : MonoBehaviour
 {
-    [SerializeField] private GameObject[] currentTeam = new GameObject[5];
+    [SerializeField] private int teamSize;
+    [SerializeField] private GameObject[] currentTeam;
+
+    private void Awake()
+    {
+        currentTeam = new GameObject[teamSize];
+    }
 
     public void AddMember(GameObject member)
     {
@@ -20,5 +26,17 @@ public class TeamManager : MonoBehaviour
     {
         currentTeam[index] = null;
     }
- 
+
+    public void ResizeArray()
+    {
+        //store old array incase u need to change it mid Post Combat
+        var oldArrayValues = currentTeam;
+        currentTeam = new GameObject[teamSize];
+        
+        //adds old values to new array so you dont resize it
+        for (int i = 0; i < oldArrayValues.Length ; i++)
+        {
+            currentTeam[i] = oldArrayValues[i];
+        }
+    }
 }
