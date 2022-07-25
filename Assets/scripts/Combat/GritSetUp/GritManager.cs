610c4fbacc71c8198a70d1fa205235fdf0f69d3a
Kazuma-47 <70643013+Kazuma-47@users.noreply.github.com>
2022-07-25 13:37:57 +0200

    safe progress

--- Assets/scripts/Combat/GritSetUp/GritManager.cs
+++ Assets/scripts/Combat/GritSetUp/GritManager.cs
@@ -10,6 +10,7 @@ public class GritManager : MonoBehaviour
     [SerializeField] private Vector3 expantionRadius;
     [SerializeField] private Tile prefab;
     private BoxCollider _collider;
+    public Vector3 bounds2;
     
     private void Start()
     {
@@ -27,8 +28,10 @@ public class GritManager : MonoBehaviour
             Instantiate(prefab,new Vector3(position.x,position.y+2,position.z), Quaternion.identity ,gameObject.transform);
         }
     }
-    //will respond to objects as a grit
-    public void GenerateResponsiveGrit()
+    //will respond to objects as a grit can be used for a island type level/large scale map etc
+    //if map is one object needs to fill it with multiple instances?
+    //otherwise this works
+    public void GenerateIslandGrit()
     {
         Collider[] Objects=  Physics.OverlapBox(gameObject.transform.position, expantionRadius, quaternion.identity);
         foreach (var surface in  Objects)
@@ -39,21 +42,36 @@ public class GritManager : MonoBehaviour
             spawnedTile.name = $"Tile";
         }
     }
-    
+
+    public void GenerateResponsiveGrit()
+    {
+        Collider[] Objects = Physics.OverlapBox(gameObject.transform.position, expantionRadius, quaternion.identity);
+        foreach (var ground in Objects)
+        {
+            var surface = ground.GetComponent<Renderer>().bounds.extents *2;
+            var placement = ground.GetComponent<Renderer>().bounds.center;
+            print(surface);
+            GenerateGrit(surface.x,1, surface.z , placement);
+            
+
+        }
+    }
+
+    //make a variable with  [SerializeField] private int width,height,depth;
     //if you want to generate a flat terain as grit
-   /* public void GeneratePlainGrit()
+    public void GenerateGrit(float width,float height,float depth , Vector3 bounds)
     {
         for(int x = 0; x < width; x++){
             for (int y = 0; y < height; y++){
                 for (int z = 0; z < depth; z++)
                 {
-                    var spawnedTile = Instantiate(prefab, new Vector3(x, y, z), Quaternion.identity ,gameObject.transform);
+                    var spawnedTile = Instantiate(prefab, new Vector3(x,y,z), Quaternion.identity ,gameObject.transform);
                     spawnedTile.name = $"Tile {x} {y}";
                 }
               
             }
         }
-    }*/
+    }
 
    public void DeleteGrit()
     {
@@ -65,6 +83,7 @@ public class GritManager : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(transform.position, expantionRadius);
+        //Gizmos.DrawWireCube(transform.position, bounds2 *2);
 
     }
 }
