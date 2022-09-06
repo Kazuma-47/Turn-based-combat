ed9052531e69ff1da7629cb3b86518125221bb55
Kazuma-47 <70643013+Kazuma-47@users.noreply.github.com>
2022-09-06 15:45:36 +0200

    fixed errors

--- Assets/scripts/Combat/TurnManager.cs
+++ Assets/scripts/Combat/TurnManager.cs
@@ -14,38 +14,23 @@ public class TurnManager : MonoBehaviour
 {
     private enum State
     {
-        PlayerTurn, 
+        PlayerTurn,
         EnemyTurn
     }
 
-    public float HpScale = 1.025f;
     public int health;
-    public int IntHP;
-    public float FloatHP;
 
     public void Start()
     {
-        GetHealth();
         print(health);
     }
+
     private void Update()
     {
         if (Input.GetKeyDown("space"))
         {
             health = health - 5;
-            BattleEnd();
         }
 
-    }
-    public void GetHealth()
-    {
-        health = CharacterStats.currentHP;
-        FloatHP = Mathf.Pow(HpScale, CharacterStats.monsterLVL) * CharacterStats.monsterHP;
-        IntHP = (int)Math.Round(FloatHP);
-    }
-    public void BattleEnd()
-    {
-        CharacterStats.currentHP = health;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
