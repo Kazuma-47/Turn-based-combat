507f363edd11d0aec2af246f040c21670a1ac8c5
Kazuma-47 <70643013+Kazuma-47@users.noreply.github.com>
2022-10-04 10:40:31 +0200

    made build replayeble

--- Assets/scripts/UI/Start.cs
+++ Assets/scripts/UI/Start.cs
@@ -7,22 +7,6 @@ public class Start : MonoBehaviour
 
     public void Play()
     {
-        PlayerPrefs.SetInt("Ketsuhealth",100);
-        PlayerPrefs.SetInt("47health",150);
-        PlayerPrefs.SetInt("Biahealth",100);
-        PlayerPrefs.SetInt("kEVINhealth", 150);
-        PlayerPrefs.SetInt("RosalinaHealth",100);
-        
-
-        PlayerPrefs.SetInt("KetsuStamina", 100);
-        PlayerPrefs.SetInt("47Stamina", 100);
-        PlayerPrefs.SetInt("BiaMana", 100);
-        PlayerPrefs.SetInt("kEVINStamina", 100);
-        PlayerPrefs.SetInt("RosalinaStamina", 100);
-
-
-        PlayerPrefs.SetInt("EnemyHealth", 3000);
-
-        SceneManager.LoadScene("battle screen");
+        SceneManager.LoadScene("EnemiePlacing");
     }
 }
