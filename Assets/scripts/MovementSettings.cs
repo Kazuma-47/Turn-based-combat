eefaa946b9943659e7297600aada5f78868b2813
Kazuma-47 <70643013+Kazuma-47@users.noreply.github.com>
2023-08-08 21:35:51 +0200

    movement 1.0 and proect added

---
+++ Assets/scripts/MovementSettings.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Movement Config")]
+public class MovementSettings : ScriptableObject
+{
+	public float jumpStrength;
+    public float maxRunSpeed;
+	public float runAcceleration;
+	public float runDecceleration;
+	[HideInInspector] public float runAccelAmount;
+    [HideInInspector] public float runDeccelAmount;
+
+	private void OnValidate()
+	{
+		runAccelAmount = (50 * runAcceleration) / maxRunSpeed;
+		runDeccelAmount = (50 * runDecceleration) / maxRunSpeed;
+	
+		runAcceleration = Mathf.Clamp(runAcceleration, 0.01f, maxRunSpeed);
+		runDecceleration = Mathf.Clamp(runDecceleration, 0.01f, maxRunSpeed);
+
+	}
+}
